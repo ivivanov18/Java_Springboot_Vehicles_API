22b344119a537e18a8e61abc845766c761887011
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2022-06-17 02:17:52 +0300

    small changes to Pricing Service: - add config for H2 console and deferred data initialization - add test dependency to project - add data.sql to seed data to table

---
+++ pricing-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO price (vehicle_id,currency,price) VALUES (1,'EUR',12645.23);
+INSERT INTO price (vehicle_id,currency,price) VALUES (2,'EUR',8000.00);
+INSERT INTO price (vehicle_id,currency,price) VALUES (3,'EUR',4500.00);
